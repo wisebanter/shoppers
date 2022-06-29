07acff553145fc7afd6294eb31aa35cd22f562ef
Salma <salmaabdurahman20@gmail.com>
2022-06-29 16:13:46 +0800

    Salma

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -12,7 +12,7 @@ class _ScreenRegisterState extends State<ScreenRegister> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Registration Page"),
+        title: Text("Registration Form"),
       ),
       body: SingleChildScrollView(
         child: Column(

9076127ac46f0273bfb1dc6a9a08a8324c99bf11
wisebanter <wisebanter5@gmail.com>
2022-06-29 12:12:49 +0300

    Change

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -17,36 +17,36 @@ class _ScreenRegisterState extends State<ScreenRegister> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const Icon(
-              Icons.account_circle,
-              size: 100,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Username",
-                hintText: "Enter Your Username",
-              ),
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: "Username",
-                hintText: "Enter Your Username",
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Container(
-                width: 200,
-                height: 50,
-                decoration: const BoxDecoration(color: Colors.blue),
-                child: const Center(
-                  child: Text(
-                    "Login",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            ),
+            // const Icon(
+            //   Icons.account_circle,
+            //   size: 100,
+            // ),
+            // const TextField(
+            //   decoration: InputDecoration(
+            //     labelText: "Username",
+            //     hintText: "Enter Your Username",
+            //   ),
+            // ),
+            // const TextField(
+            //   decoration: InputDecoration(
+            //     labelText: "Username",
+            //     hintText: "Enter Your Username",
+            //   ),
+            // ),
+            // TextButton(
+            //   onPressed: () {},
+            //   child: Container(
+            //     width: 200,
+            //     height: 50,
+            //     decoration: const BoxDecoration(color: Colors.blue),
+            //     child: const Center(
+            //       child: Text(
+            //         "Login",
+            //         style: TextStyle(color: Colors.white),
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ],
         ),
       ),
